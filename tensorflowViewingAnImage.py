593dcb1de55900fd9855d7a662334790f7f1308c
Graeme Crawley <gacrawley@gmail.com>
2017-02-20 23:33:25 -0500

    Now able to view numbers. Next will be finding ten two's and changing their individual values.. I guess...

--- tensorflowViewingAnImage.py
+++ tensorflowViewingAnImage.py
@@ -1,36 +1,41 @@
-#from PIL import Image
 import numpy as np
 from binascii import hexlify
-
-#Below snippet retrieved from:
-#http://stackoverflow.com/questions/2659312/how-do-i-convert-a-numpy-array-to-and-display-an-image
-
-w, h = 512, 512
-data = np.zeros((h, w, 3), dtype=np.uint8)
-data[256, 256] = [255, 0, 0]
-img = Image.fromarray(data, 'RGB')
-img.save('my.png')
-img.show()
-
-
+import matplotlib.pyplot as plt
 
 #Following code snippet retrieved from:
 #http://stackoverflow.com/questions/1035340/reading-binary-file-in-python-and-looping-over-each-byte
+
 i=0
 x=0
 y=0
+w, h = 28, 28
+data = np.zeros((h, w, 3), dtype=np.uint8)
 numberOfBytesAllowing = 784
 with open("OnePieceOfData/t10k-images.idx3-ubyte", "rb") as f:
+	#Used to pass over the first 16 bytes of the file as it's (right now) unnecessary data
+    byte = f.seek(15)
     byte = f.read(1)
+
     while byte != "":
-        #print("Byte number:",i,"Byte",hex(byte))
         hexifiedByte = hexlify(byte)
-        print int(hexifiedByte, 16)
+        #print int(hexifiedByte, 16)
+        data[y][x][0] = int(hexifiedByte, 16)
         byte = f.read(1)
-        if(i==numberOfBytesAllowing):
+        x+=1
+        if(x==28):
+        	y+=1
+        	x=0
+        if(y==28):
         	break
         else:
        		i+=1
+
+#I used pyplot to show the image as it presented it in the way I liked best. Source below:
+#http://stackoverflow.com/questions/13811334/saving-numpy-ndarray-in-python-as-an-image
+
+plt.imshow(data) 
+plt.show()
+plt.savefig("array")
 
 
 
