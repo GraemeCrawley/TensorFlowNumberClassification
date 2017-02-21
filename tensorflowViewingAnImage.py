acff2b19b976cb8cfe48d7dd7785dc97cb079029
Graeme Crawley <gacrawley@gmail.com>
2017-02-21 00:07:16 -0500

    was able to save images as numbers in a folder. Moving on to finding 2's that have been correctly classified. After this, I'll save them to a folder, and start playing with the modifications.

--- tensorflowViewingAnImage.py
+++ tensorflowViewingAnImage.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 #http://stackoverflow.com/questions/1035340/reading-binary-file-in-python-and-looping-over-each-byte
 
 i=0
+imageNum=0
 x=0
 y=0
 w, h = 28, 28
@@ -13,28 +14,32 @@ data = np.zeros((h, w, 3), dtype=np.uint8)
 numberOfBytesAllowing = 784
 with open("OnePieceOfData/t10k-images.idx3-ubyte", "rb") as f:
 	#Used to pass over the first 16 bytes of the file as it's (right now) unnecessary data
-    byte = f.seek(15)
+    byte = f.seek(16)
     byte = f.read(1)
-
-    while byte != "":
-        hexifiedByte = hexlify(byte)
-        #print int(hexifiedByte, 16)
-        data[y][x][0] = int(hexifiedByte, 16)
-        byte = f.read(1)
-        x+=1
-        if(x==28):
-        	y+=1
-        	x=0
-        if(y==28):
-        	break
-        else:
-       		i+=1
+    while(imageNum<50):  
+        while byte != "":
+            hexifiedByte = hexlify(byte)
+            #print int(hexifiedByte, 16)
+            data[y][x][0] = int(hexifiedByte, 16)
+            byte = f.read(1)
+            x+=1
+            if(x==28):
+            	y+=1
+            	x=0
+            if(y==28):
+            	break
+            else:
+           		i+=1
+        plt.imshow(data) 
+        plt.savefig("CreatedImages/array"+str(imageNum))
+        imageNum+=1
+        y=0
 
 #I used pyplot to show the image as it presented it in the way I liked best. Source below:
 #http://stackoverflow.com/questions/13811334/saving-numpy-ndarray-in-python-as-an-image
 
 plt.imshow(data) 
-plt.show()
+#plt.show()
 plt.savefig("array")
 
 
