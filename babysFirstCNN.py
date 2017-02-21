09f3fde127f0c52938efde31b82aaed43d4c19df
Graeme Crawley <gacrawley@gmail.com>
2017-02-20 20:20:08 -0500

    Was able to get the tutorial working at 100% accuracy by batching the images as suggested in an issue on the tensorflow github. Next, I need to modify 10 images of a 2 so that the model recognizes them as a 6. I'll continue reading 'Breaking Linear Classifiers on ImageNet' to see if I can get a better idea on how to do that

--- babysFirstCNN.py
+++ babysFirstCNN.py
@@ -1,5 +1,6 @@
 #Importing tensoflow
 import tensorflow as tf
+import time as t
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
@@ -82,13 +83,28 @@ correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
 
-for i in range(1000):
-  batch = mnist.train.next_batch(50)
+for i in range(10000):
+  batch = mnist.train.next_batch(10)
   if i%100 == 0:
     train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g"%(i, train_accuracy))
   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+#Issue with memory and printing the below line (found out by removing the below line and trying without). Going to try to wait and see if it helps...?
+#t.sleep(5)
+#Did not work. Will now separate out the line to see where the problem is.
+#print("test accuracy %g" %accuracy.eval(feed_dict={
+#   x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+#This seems relevant, trying it:
+#https://github.com/tensorflow/tensorflow/issues/136
+
+
+print("Calculating accuracy")
+
+batch_tx, batch_ty = mnist.test.next_batch(10)
+
+accuracy = accuracy.eval(feed_dict={x: batch_tx, y_:batch_ty, keep_prob: 1.0})
+
+print("test accuracy",accuracy)
